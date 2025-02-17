591eb0522ac875ea429ec35d6417aa750eff2e33
RikiRdz1 <144867124+RikiRdz1@users.noreply.github.com>
2025-02-17 00:39:19 -0600

    Proyecto tipos de datos de JavaScript.

---
+++ main.js
@@ -0,0 +1,28 @@
+//Tipos de datos en JavaScript
+
+console.log(42);
+console.log(typeof 42);
+
+console.log('Veinticinco');
+console.log(typeof 'Veinticinco');
+
+console.log(-666);
+console.log(typeof -666);
+
+console.log(true);
+console.log(typeof true);
+
+console.log(0);
+console.log(typeof 0);
+
+console.log("");
+console.log(typeof "");
+
+console.log(null);
+console.log(typeof null);
+
+console.log(undefined);
+console.log(typeof undefined);
+
+console.log(false);
+console.log(typeof false);
